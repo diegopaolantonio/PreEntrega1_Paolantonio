d902fe49bbc21dde41822abe638086b8d7973145
diegopaolantonio <dpaolantonio.expertoing@gmail.com>
2022-09-27 13:07:30 -0300

    Commit 2, corregi textos, y agrege css

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,7 @@ let cantidad = 0;
 let opcionTiempo = 0;
 let dias = 0;
 let precio = 0;
+let herramientas = 0;
 let costo = 0;
 let horas = 0;
 let trabajoString;
@@ -29,6 +30,7 @@ function costoHora(trabajo) {
     precio = 0;
     trabajoString = "Indefinido";
   }
+
   return precio;
 }
 
@@ -52,7 +54,38 @@ do {
   );
 } while (controlarNumero(trabajo, 1, 4));
 
+do {
+  if (trabajo == 1) {
+    herramientas = String(prompt("Llevar computadora propia?"));
+  } else if (trabajo == 2) {
+    herramientas = String(prompt("Llevar herramientas propias?"));
+  } else if (trabajo == 3) {
+    alert("No se asiste con herramientas ni computadora");
+    herramientas = "no";
+  }
+
+  if (
+    herramientas == "si" ||
+    herramientas == "Si" ||
+    herramientas == "SI" ||
+    herramientas == "sI"
+  ) {
+    herramientas = "Si";
+  } else if (
+    herramientas == "no" ||
+    herramientas == "No" ||
+    herramientas == "NO" ||
+    herramientas == "nO"
+  ) {
+    herramientas = "No";
+  }
+} while (herramientas != "Si" && herramientas != "No");
+
 precio = costoHora(trabajo);
+
+if (herramientas == "Si") {
+  precio = precio * 1.25;
+}
 
 if (trabajo != 4) {
   do {
@@ -85,18 +118,22 @@ if (trabajo != 4) {
   costo = dias * precio * cantidad * horas;
 }
 
-console.log("personal: " + cantidad);
-console.log("Precio hora de trabajo " + precio);
+console.log("Cantidad de personas: " + cantidad);
+console.log("Computadora/Herramientas propias: " + herramientas);
 console.log("Cantidad de dias " + dias);
 console.log("Horas de trabajo por dia " + horas);
+console.log("Precio hora de trabajo " + precio);
 console.log("Costo total " + costo);
 
-document.write("<h2>El costo total es de: USD " + costo + ",00</h2>");
 document.write("<p>Datos introducidos:</p>");
 document.write("<ul>");
 document.write("<li>Tipo de trabajo: " + trabajoString + "</li>");
 document.write("<li>Cantidad de personas: " + cantidad + "</li>");
+document.write(
+  "<li>Computadora/Herramientas propias: " + herramientas + "</li>"
+);
 document.write("<li>Cantidad de dias: " + dias + "</li>");
 document.write("<li>Jornada laboral de " + horas + " horas</li>");
-document.write("<li>Precio por hora USD " + precio + ",00</li>");
+document.write("<li>Precio por hora USD " + precio + "</li>");
 document.write("</ul>");
+document.write("<h2>El costo total es de: USD " + costo + "</h2>");
